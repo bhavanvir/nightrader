cc822229429b847e9752b38bd77e0c3396769401
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-01-30 20:02:36 -0800

    Add frontend components (#7)
    * Remove mui
    
    * Add and configure daisyui
    
    * Add frontend skeleton
    
    * Update docker-compose to include frontend
    
    * Update yaml

---
+++ Client/app/src/components/Header.jsx
@@ -0,0 +1,82 @@
+export default function Header() {
+  return (
+    <div className="navbar bg-base-100">
+      <div class="flex-1">
+        <a className="btn btn-ghost text-xl">
+          <svg
+            viewBox="0 0 200 200"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-8 h-8 stroke-current"
+          >
+            <g clip-path="url(#clip0_381_1024)">
+              <circle cx="100" cy="100" r="88" stroke-width="24" />
+              <mask
+                id="mask0_381_1024"
+                style={{ maskType: "alpha" }}
+                maskUnits="userSpaceOnUse"
+                x="0"
+                y="0"
+                width="200"
+                height="201"
+              >
+                <circle cx="100" cy="100" r="100" fill="black" />
+              </mask>
+              <g mask="url(#mask0_381_1024)">
+                <circle
+                  cx="66.1763"
+                  cy="58.824"
+                  r="74.7647"
+                  stroke-width="24"
+                />
+              </g>
+            </g>
+            <defs>
+              <clipPath id="clip0_381_1024">
+                <rect
+                  width="200"
+                  height="200"
+                  transform="translate(0 0.000488281)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+          <span className="font-bold">Nightrader</span>
+        </a>
+      </div>
+      <div className="flex-none ">
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
