29e7e85e38c8e4abf632f53ac022ef42439a8e6d
bhavanvir <bhavanvir.r@gmail.com>
2024-01-30 18:24:11 -0800

    Add frontend skeleton

--- Client/app/src/App.js
+++ Client/app/src/App.js
@@ -1,30 +1,13 @@
-import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch("http://localhost:8080/users");
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-
-    fetchUsers();
-  }, []);
-
   return (
     <div className="App">
-      <h1>React App</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <Header />
+      <Hero />
+      <Footer />
     </div>
   );
 }
---
+++ Client/app/src/components/Hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <h1 className="text-5xl font-bold">Great things coming soon.</h1>
+      </div>
+    </div>
+  );
+}
