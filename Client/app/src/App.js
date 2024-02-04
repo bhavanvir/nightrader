d21ce691d808be5fcd5438a48bd4273dbbb1ddc4
bhavanvir <bhavanvir.r@gmail.com>
2024-02-04 00:00:29 -0800

    Add page routing for home/sign in page

--- Client/app/src/App.js
+++ Client/app/src/App.js
@@ -1,13 +1,14 @@
-// App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import SignIn from "./components/SignIn/SignIn";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
   );
