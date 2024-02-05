64c4fdfa0f2f3e7814cf5f63ea1e01619d4fc6ac
bhavanvir <bhavanvir.r@gmail.com>
2024-02-04 20:09:27 -0800

    Change service casing to lowercase

---
+++ client/app/src/components/Home/Hero.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+export default function Hero() {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content grid grid-rows-2 text-center">
+        <div>
+          <h1 className="text-5xl font-bold">
+            Take control of your financial future.
+          </h1>
+          <h2 className="text-2xl">
+            Sign up for early access to our all new trading platform.
+          </h2>
+        </div>
+
+        <div className="justify-start">
+          <Link to="/signin">
+            <button className="btn btn-primary btn-wide btn-md m-4">
+              Sign in
+            </button>
+          </Link>
+          <button className="btn btn-primary btn-outline btn-wide btn-md">
+            Sign up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ client/app/src/components/SignIn/SignIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./SignInForm";
+
+const SignIn = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
+
+export default SignIn;
