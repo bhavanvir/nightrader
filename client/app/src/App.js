1a211354b73825e2724bb3c1d8de1b30cbcaed0e
bhavanvir <bhavanvir.r@gmail.com>
2024-02-10 16:44:10 -0800

    Add support for private routes and cookie retrival

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,20 +1,83 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
 
 import Home from "./components/Home/Home";
 import SignIn from "./components/SignIn/SignIn";
 import SignUp from "./components/SignUp/SignUp";
+import Dashboard from "./components/Dashboard/Dashboard";
+import AuthApi from "./AuthApi";
 
 function App() {
+  const [auth, setAuth] = React.useState(false);
+  const [user, setUser] = React.useState({});
+
+  useEffect(() => {
+    const readCookie = () => {
+      const sessionToken = Cookies.get("session_token");
+      if (sessionToken) {
+        const decodedUser = jwtDecode(sessionToken);
+        setUser(decodedUser);
+        setAuth(true);
+      }
+    };
+
+    readCookie();
+  }, []);
+
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/signin" element={<SignIn />} />
-        <Route exact path="/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <AuthApi.Provider value={{ auth, setAuth }}>
+      <Router>
+        <AllRoutes user={user} />
+      </Router>
+    </AuthApi.Provider>
   );
 }
+
+const AllRoutes = ({ user }) => {
+  const Auth = React.useContext(AuthApi);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/signin"
+        element={<ProtectedLogin auth={Auth.auth} element={<SignIn />} />}
+      />
+      <Route path="/signup" element={<SignUp />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute
+            auth={Auth.auth}
+            element={<Dashboard user={user} />}
+          />
+        }
+      />
+    </Routes>
+  );
+};
+
+const ProtectedRoute = ({ auth, element }) => {
+  if (!auth) {
+    return <Navigate to="/signin" />;
+  }
+
+  return element;
+};
+
+const ProtectedLogin = ({ auth, element }) => {
+  if (auth) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  return element;
+};
 
 export default App;
