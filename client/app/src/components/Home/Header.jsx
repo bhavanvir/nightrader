698170aaadbebd1fc3442e97332b63766f6dc1c7
bhavanvir <bhavanvir.r@gmail.com>
2024-02-10 16:44:40 -0800

    Remove user information from homepage header

--- client/app/src/components/Home/Header.jsx
+++ client/app/src/components/Home/Header.jsx
@@ -9,39 +9,6 @@ export default function Header() {
           <span className="font-bold">Nightrader</span>
         </a>
       </div>
-      <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a href="/" className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a href="/">Settings</a>
-            </li>
-            <li>
-              <a href="/">Logout</a>
-            </li>
-          </ul>
-        </div>
-      </div>
     </div>
   );
 }
