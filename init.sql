f718499b40917edd16062da7b54cbe3727f7addf
JoshuaSivertson <joshsiv@shaw.ca>
2024-02-10 22:43:43 -0800

    function trigger for password encryption

--- init.sql
+++ init.sql
@@ -1,12 +1,25 @@
 CREATE TABLE IF NOT EXISTS users (
-    user_name text primary key,
-    name text,
-    password text,
-    wallet integer
+    user_id TEXT PRIMARY KEY,
+    user_name TEXT,
+    user_pass VARCHAR(100) NOT NULL,
+    wallet NUMERIC(12,2) DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS stocks (
-    stock_id serial unique primary key,
-    stock_name text,
-    current_price integer
+    stock_id SERIAL UNIQUE PRIMARY KEY,
+    stock_name TEXT,
+    current_price NUMERIC(12,2) DEFAULT 0
 );
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE OR REPLACE FUNCTION pass_encrypt() RETURNS TRIGGER AS $$
+    BEGIN
+    NEW.user_pass = crypt(NEW.user_pass, gen_salt('md5'));
+    RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER login
+BEFORE INSERT OR UPDATE ON users
+FOR EACH ROW EXECUTE FUNCTION pass_encrypt();
