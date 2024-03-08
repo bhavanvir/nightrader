79f310db892d1f5bfee2a26e7dd0cb3f8a165c0b
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2024-03-08 22:38:39 +0000

    Remove empty string check

--- transaction/main.go
+++ transaction/main.go
@@ -349,9 +349,6 @@ func getStockTransactions(c *gin.Context) {
 			handleError(c, http.StatusInternalServerError, "Failed to scan row", err)
 			return
 		}
-		if *item.WalletTxID == "" {
-			item.WalletTxID = nil
-    }
 		fmt.Println(item)
 		stock_transactions = append(stock_transactions, item)
 	}
