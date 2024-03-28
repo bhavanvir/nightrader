b9e68ef5fc5214c54a4ef1674bf6696ee4db7115
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2024-03-27 19:38:59 -0700

    Added connection pool to authentication (#126)

--- authentication/main.go
+++ authentication/main.go
@@ -1,212 +1,191 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"net/http"
-	"time"
+    "database/sql"
+    "fmt"
+    "net/http"
+    "time"
 
-	"github.com/Poomon001/day-trading-package/identification"
-	"github.com/Poomon001/day-trading-package/tester"
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
+    "github.com/dgrijalva/jwt-go"
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
+    _ "github.com/lib/pq"
 )
+
+// Global variable for the database connection
+var db *sql.DB
 
 // TODO: need env to store secret key
 var secretKey = []byte("secret")
 
 const (
-	host = "database"
-	// host     = "localhost" // for local testing
-	port     = 5432
-	user     = "nt_user"
-	password = "db123"
-	dbname   = "nt_db"
+    host = "database"
+    port     = 5432
+    user     = "nt_user"
+    password = "db123"
+    dbname   = "nt_db"
 )
 
 type ErrorResponse struct {
-	Success bool              `json:"success"`
-	Data    map[string]string `json:"data"`
+    Success bool              `json:"success"`
+    Data    map[string]string `json:"data"`
 }
 
-// user_name is a primary key in the DB used to identify user
 type Register struct {
-	UserName string `json:"user_name"`
-	Name     string `json:"name"`
-	Password string `json:"password"`
+    UserName string `json:"user_name"`
+    Name     string `json:"name"`
+    Password string `json:"password"`
 }
 
 type Login struct {
-	UserName string `json:"user_name"`
-	Password string `json:"password"`
+    UserName string `json:"user_name"`
+    Password string `json:"password"`
 }
 
 type Response struct {
-	Success bool                   `json:"success"`
-	Data    map[string]interface{} `json:"data"`
+    Success bool                   `json:"success"`
+    Data    map[string]interface{} `json:"data"`
 }
 
 type Claims struct {
-	Name     string `json:"name"`
-	UserName string `json:"user_name"`
-	jwt.StandardClaims
+    Name     string `json:"name"`
+    UserName string `json:"user_name"`
+    jwt.StandardClaims
 }
 
 func handleError(c *gin.Context, statusCode int, message string, err error) {
-	errorResponse := ErrorResponse{
-		Success: false,
-		Data:    map[string]string{"error": message},
-	}
-	c.IndentedJSON(statusCode, errorResponse)
+    errorResponse := ErrorResponse{
+        Success: false,
+        Data:    map[string]string{"error": message},
+    }
+    c.IndentedJSON(statusCode, errorResponse)
 }
 
 func createToken(name string, username string, expirationTime time.Time) (string, error) {
-	claims := &Claims{
-		Name:     name,
-		UserName: username,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expirationTime.Unix(),
-		},
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString(secretKey)
+    claims := &Claims{
+        Name:     name,
+        UserName: username,
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: expirationTime.Unix(),
+        },
+    }
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString(secretKey)
 }
 
 func postLogin(c *gin.Context) {
-	var login Login
+    var login Login
 
-	// Verify request body
-	if err := c.BindJSON(&login); err != nil {
-		handleError(c, http.StatusBadRequest, "Invalid request body", err)
-		return
-	}
+    if err := c.BindJSON(&login); err != nil {
+        handleError(c, http.StatusBadRequest, "Invalid request body", err)
+        return
+    }
 
-	postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+    fmt.Println("Using global database connection")
 
-	db, err := sql.Open("postgres", postgresqlDbInfo)
-	if err != nil {
-		handleError(c, http.StatusInternalServerError, "Failed to connect to the database", err)
-		return
-	}
-	defer db.Close()
+    var count int
+    err := db.QueryRow("SELECT COUNT(*) FROM users WHERE user_name = $1", login.UserName).Scan(&count)
+    if err != nil {
+        handleError(c, http.StatusInternalServerError, "Failed to query the database", err)
+        return
+    }
+    if count == 0 {
+        handleError(c, http.StatusBadRequest, "Username does not exist", nil)
+        return
+    }
 
-	fmt.Println("Successfully connected to the database")
+    var correctPassword bool
+    err = db.QueryRow("SELECT (user_pass = crypt($1, user_pass)) AS is_valid FROM users WHERE user_name = $2", login.Password, login.UserName).Scan(&correctPassword)
+    if err != nil {
+        handleError(c, http.StatusInternalServerError, "Failed to query the database", err)
+        return
+    }
+    if !correctPassword {
+        handleError(c, http.StatusOK, "Incorrect password", nil)
+        return
+    }
 
-	// Check if the username exists in DB
-	var count int
-	err = db.QueryRow("SELECT COUNT(*) FROM users WHERE user_name = $1", login.UserName).Scan(&count)
-	if err != nil {
-		handleError(c, http.StatusInternalServerError, "Failed to query the database", err)
-		return
-	}
-	if count == 0 {
-		handleError(c, http.StatusBadRequest, "Username does not exist", nil)
-		return
-	}
+    var name string
+    err = db.QueryRow("SELECT name FROM users WHERE user_name = $1", login.UserName).Scan(&name)
+    if err != nil {
+        handleError(c, http.StatusInternalServerError, "Failed to query the database", err)
+        return
+    }
 
-	// Check password for the username
-	var correctPassword bool
+    minutes := 30 * time.Minute
+    expirationTime := time.Now().Add(minutes)
+    token, err := createToken(name, login.UserName, expirationTime)
+    if err != nil {
+        handleError(c, http.StatusInternalServerError, "Failed to create token", err)
+        return
+    }
 
-	err = db.QueryRow("SELECT (user_pass = crypt($1, user_pass)) AS is_valid FROM users WHERE user_name = $2", login.Password, login.UserName).Scan(&correctPassword)
-	if err != nil {
-		handleError(c, http.StatusInternalServerError, "Failed to query the database", err)
-		return
-	}
-	if !correctPassword {
-		// Update to return a 200 error if the password is incorrect
-		handleError(c, http.StatusOK, "Incorrect password", nil)
-		return
-	}
+    loginResponse := Response{
+        Success: true,
+        Data:    map[string]interface{}{"token": token},
+    }
 
-	// Get the user's name
-	var name string
-	err = db.QueryRow("SELECT name FROM users WHERE user_name = $1", login.UserName).Scan(&name)
-	if err != nil {
-		handleError(c, http.StatusInternalServerError, "Failed to query the database", err)
-		return
-	}
-
-	// Create token
-	minutes := 30 * time.Minute
-	expirationTime := time.Now().Add(minutes)
-	token, err := createToken(name, login.UserName, expirationTime)
-	if err != nil {
-		handleError(c, http.StatusInternalServerError, "Failed to create token", err)
-		return
-	}
-
-	// Respond
-	loginResponse := Response{
-		Success: true,
-		Data:    map[string]interface{}{"token": token},
-	}
-
-	c.IndentedJSON(http.StatusOK, loginResponse)
+    c.IndentedJSON(http.StatusOK, loginResponse)
 }
 
 func postRegister(c *gin.Context) {
-	postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+    var newRegister Register
 
-	db, err := sql.Open("postgres", postgresqlDbInfo)
-	if err != nil {
-		handleError(c, http.StatusInternalServerError, "Failed to connect to the database", err)
-		return
-	}
-	defer db.Close()
+    if err := c.BindJSON(&newRegister); err != nil {
+        handleError(c, http.StatusBadRequest, "Invalid request body", err)
+        return
+    }
 
-	fmt.Println("Successfully connected to the database")
+    fmt.Println("Using global database connection")
 
-	var newRegister Register
+    var count int
+    err := db.QueryRow("SELECT COUNT(*) FROM users WHERE user_name = $1", newRegister.UserName).Scan(&count)
+    if err != nil {
+        handleError(c, http.StatusInternalServerError, "Failed to query the database", err)
+        return
+    }
+    if count > 0 {
+        handleError(c, http.StatusOK, "Username already exists", nil)
+        return
+    }
 
-	if err := c.BindJSON(&newRegister); err != nil {
-		handleError(c, http.StatusBadRequest, "Invalid request body", err)
-		return
-	}
+    _, err = db.Exec("INSERT INTO users (user_name, name, user_pass) VALUES ($1, $2, $3)", newRegister.UserName, newRegister.Name, newRegister.Password)
+    if err != nil {
+        handleError(c, http.StatusInternalServerError, "Failed to insert new user to the database", err)
+        return
+    }
 
-	// Check if the username already exists in DB
-	var count int
-	err = db.QueryRow("SELECT COUNT(*) FROM users WHERE user_name = $1", newRegister.UserName).Scan(&count)
-	if err != nil {
-		handleError(c, http.StatusInternalServerError, "Failed to query the database", err)
-		return
-	}
-	if count > 0 {
-		// Return 200 StatusOK if the username already exists
-		handleError(c, http.StatusOK, "Username already exists", nil)
-		return
-	}
+    successResponse := Response{
+        Success: true,
+        Data:    nil,
+    }
 
-	// Insert new user to DB
-	_, err = db.Exec("INSERT INTO users (user_name, name, user_pass) VALUES ($1, $2, $3)", newRegister.UserName, newRegister.Name, newRegister.Password)
-	if err != nil {
-		handleError(c, http.StatusInternalServerError, "Failed to insert new user to the database", err)
-		return
-	}
-
-	// Format JSON response
-	successResponse := Response{
-		Success: true,
-		Data:    nil,
-	}
-
-	c.IndentedJSON(http.StatusCreated, successResponse)
+    c.IndentedJSON(http.StatusCreated, successResponse)
 }
 
 func main() {
-	router := gin.Default()
+    postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+    var err error
+    db, err = sql.Open("postgres", postgresqlDbInfo)
+    if err != nil {
+        fmt.Printf("Failed to connect to the database: %v\n", err)
+        return
+    }
+    defer db.Close()
 
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000"}
-	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "token"}
-	config.AllowCredentials = true
-	router.Use(cors.New(config))
+    db.SetMaxOpenConns(100) // Set maximum number of open connections
+    db.SetMaxIdleConns(5) // Set maximum number of idle connections
 
-	identification.Test()
-	tester.TestUser()                                               // example how to use function from a package
-	router.POST("/login", identification.TestMiddleware, postLogin) // example how to use middlware from a package
-	router.POST("/register", postRegister)
-	router.Run(":8888")
+    router := gin.Default()
+
+    config := cors.DefaultConfig()
+    config.AllowOrigins = []string{"http://localhost:3000"}
+    config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
+    config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "token"}
+    config.AllowCredentials = true
+    router.Use(cors.New(config))
+
+    router.POST("/login", postLogin)
+    router.POST("/register", postRegister)
+    router.Run(":8888")
 }
