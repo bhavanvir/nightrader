0bd45207064a110e10cb1c3132be6c9d47f1bd24
JoshuaSivertson <joshsiv@shaw.ca>
2024-04-05 21:13:42 -0700

    DB logical partitioning

--- init.sql
+++ init.sql
@@ -26,7 +26,14 @@ CREATE TABLE IF NOT EXISTS wallet_transactions (
     is_debit BOOLEAN,
     amount NUMERIC(20,2),
     time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+) PARTITION BY HASH(wallet_tx_id);
+
+CREATE TABLE IF NOT EXISTS wallet_transactions_h0 PARTITION OF wallet_transactions FOR VALUES WITH (modulus 4, remainder 0);
+CREATE TABLE IF NOT EXISTS wallet_transactions_h1 PARTITION OF wallet_transactions FOR VALUES WITH (modulus 4, remainder 1);
+CREATE TABLE IF NOT EXISTS wallet_transactions_h2 PARTITION OF wallet_transactions FOR VALUES WITH (modulus 4, remainder 2);
+CREATE TABLE IF NOT EXISTS wallet_transactions_h3 PARTITION OF wallet_transactions FOR VALUES WITH (modulus 4, remainder 3);
+
+CREATE INDEX IF NOT EXISTS wallet_tx_idx ON wallet_transactions USING HASH (user_name);
 
 CREATE TABLE IF NOT EXISTS stock_transactions (
     stock_tx_id TEXT UNIQUE PRIMARY KEY,
@@ -40,7 +47,14 @@ CREATE TABLE IF NOT EXISTS stock_transactions (
     stock_price NUMERIC(20,2) NOT NULL,
     quantity NUMERIC(20,2),
     time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+) PARTITION BY HASH(stock_tx_id);
+
+CREATE TABLE IF NOT EXISTS stock_transactions_h0 PARTITION OF stock_transactions FOR VALUES WITH (modulus 4, remainder 0);
+CREATE TABLE IF NOT EXISTS stock_transactions_h1 PARTITION OF stock_transactions FOR VALUES WITH (modulus 4, remainder 1);
+CREATE TABLE IF NOT EXISTS stock_transactions_h2 PARTITION OF stock_transactions FOR VALUES WITH (modulus 4, remainder 2);
+CREATE TABLE IF NOT EXISTS stock_transactions_h3 PARTITION OF stock_transactions FOR VALUES WITH (modulus 4, remainder 3);
+
+CREATE INDEX IF NOT EXISTS stock_tx_idx ON stock_transactions USING HASH (user_name);
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
